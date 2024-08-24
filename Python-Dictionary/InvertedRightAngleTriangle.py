b40460fe769dd9a22be13110dfc2fceca0485d27
Sahil Satija <96464952+codebysahil@users.noreply.github.com>
2024-08-24 12:57:04 +0200

    Create InvertedRightAngleTriangle.py

---
+++ Python-Dictionary/InvertedRightAngleTriangle.py
@@ -0,0 +1,17 @@
+# wap to print inverted right angled triangle
+
+# Enter the no of rows for the triangle  4
+# ****
+# ***
+# **
+# *
+
+
+
+
+# inverted right angle triangle
+
+
+n = int(input("Enter the no of rows for the triangle"))
+for i in range(n,0,-1):
+    print("*" * i)
